53ffcfb6a58ca815644f34bb68c9f9cb693bf237
jedi9t <harling909@gmail.com>
2026-02-07 11:11:25 +0800

    OAuth google-8

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -25,6 +25,7 @@ import { NextResponse } from 'next/server';
 import { createClient } from '@/lib/supabase/server';
 import { cookies } from 'next/headers';
 
+export const runtime = 'edge';
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
